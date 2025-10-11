14460727075eacdcbce76256da4eca1d2e93c651
iturnbul <iturnbul@iwu.edu>
2025-10-11 09:52:01 -0500

    added a category column to the db and for the user

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
+    cur = db.execute('select title, text, categories from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -75,8 +75,9 @@ def show_entries():
 @app.route('/add', methods=['POST'])
 def add_entry():
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, text, categories) values (?, ?, ?)',
+               [request.form['title'], request.form['text'], request.form['categories']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
